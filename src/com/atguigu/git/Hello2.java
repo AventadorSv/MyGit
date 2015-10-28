71d93a619ca394d6b708a24163f5e1d459c50b66
AventadorSv <aventador_740@163.com>
2015-10-28 23:17:26 +0800

    修改本地!

--- src/com/atguigu/git/Hello2.java
+++ src/com/atguigu/git/Hello2.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello2!");
+		System.out.println("hello!");
+		System.out.println("改动本地!");
 	}
 }
